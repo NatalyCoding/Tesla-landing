80a285dbe8b718797f74f6bbf49ae8f1697b0547
Nataly Panakhyd <natalycoding@gmail.com>
2020-03-23 15:08:01 +0200

    fix: slider dots below menu

--- js/main.js
+++ js/main.js
@@ -8,6 +8,8 @@ $(function() {
 
     $('.header-btn').on('click', function() {
         $('.menu').addClass('active');
+        $('.menu').addClass('z-index', '7');
+        $('.slick-dots').css('z-index', '0');
     });
 
     $('.close-btn').on('click', function() {
